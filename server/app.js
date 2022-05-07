18f6a3a98dcf5e9c477e446abfa862f87039492b
姬恒 <437462784@qq.com>
2022-05-07 14:57:09 +0800

    add: wechat card

--- server/app.js
+++ server/app.js
@@ -12,16 +12,16 @@ const port = 9000;
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log('connect db-words success');
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");  // 允许跨域
-        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");   // 允许的方法
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");   // 允许接收这些头
-        next();
-    });
-    app.use(bodyParser.json());
-    app.use('/', router);
-    app.listen(port);
-    console.log('server listen at port : ' + port);    
+db.once('open', function () {
+  console.log('connect db-words success');
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");  // 允许跨域
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");   // 允许的方法
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");   // 允许接收这些头
+    next();
+  });
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use('/', router);
+  app.listen(port);
+  console.log('server listen at port : ' + port);
 });
